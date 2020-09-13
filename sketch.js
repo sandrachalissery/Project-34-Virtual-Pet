a6b7a85f56a1c1c721960399e76818a94e4e7bd2
sandrachalissery <65679179+sandrachalissery@users.noreply.github.com>
2020-09-13 15:57:02 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,58 @@
+//Create variables here
+var dog, happyDog;
+var dogImg;
+var database;
+var foodS, foodStock;
+
+function preload()
+{
+  dogImg = loadImage("Dog.png");
+  happyDog= loadImage("happydog.png");
+}
+
+function setup() {
+  
+  createCanvas(500, 500);
+  
+  dog=createSprite(250,300,150,150);
+  dog.addImage(dogImg);
+  dog.scale = 0.15;
+
+  
+  database=firebase.database();
+
+  foodStock=database.ref('Food');
+  foodStock.on("value",readStock);
+
+}
+
+
+function draw() {  
+  background(46,139,87);
+
+  if (keyWentDown(UP_ARROW)){
+    writeStock(foodS);
+    dog.addImage(happyDog);
+  }
+  drawSprites();
+    fill("white");
+    textSize(13);
+    text("Note: press the up key to feed the doggo Milk",200,450);
+  
+}
+
+function readStock(data){
+  foodS=data.val();
+}
+
+function writeStock(x){
+  if(x<=0){
+    x-0;
+  }else{
+    x=x-1;
+  }
+  
+  database.ref('/').update({
+    Food:x
+  })
+}
